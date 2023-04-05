7202215876c0deff729b0e88388376977290a7ae
vietmy161003 <vietmy16102003@gmail.com>
2023-04-05 17:40:43 +0700

    Add rails admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :wallet_details
   resources :wallets
   resources :customer_informations
